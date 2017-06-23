03048b0b53c3b567c384a7c8b3c4aa8eaa364c97
Hoang Pham <hoangpx110488@gmail.com>
2017-06-23 15:58:44 -0600

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -18,22 +18,22 @@ gulp.task("resources", ["server", "app", "assets"], function () {
 });
 /* copy the app core files to the build folder */
 gulp.task("app", ['index'], function(){
-    return gulp.src(["app/**", "!app/**/*.ts"])
+    return gulp.src(["src/app/**", "!src/app/**/*.ts"])
         .pipe(gulp.dest("build/app"));
 });
 /* get the index file to the root of the build */
 gulp.task("index", function(){
-    return gulp.src(["index.html"])
+    return gulp.src(["src/index.html"])
         .pipe(gulp.dest("build"));
 });
 /* copy node server to build folder */
 gulp.task("server", function () {
-    return gulp.src(["index.js", "package.json"], { cwd: "server/**" })
+    return gulp.src(["index.js", "package.json"], { cwd: "src/server/**" })
         .pipe(gulp.dest("build"));
 });
 /* styles and other assets */
 gulp.task("assets", function(){
-    return gulp.src(["styles.css"])
+    return gulp.src(["src/*.css", "src/*.js", "src/*.ico", "src/*.json"])
         .pipe(gulp.dest("build"));
 });
 /**
@@ -41,14 +41,18 @@ gulp.task("assets", function(){
  */
 gulp.task("libs", function () {
     return gulp.src([
-        'es6-shim/es6-shim.min.js',
-        'systemjs/dist/system-polyfills.js',
-        'angular2/bundles/angular2-polyfills.js',
-        'angular2/es6/dev/src/testing/shims_for_IE.js',
+        'core-js/client/shim.min.js',
         'systemjs/dist/system.src.js',
-        'rxjs/bundles/Rx.js',
-        'angular2/bundles/angular2.dev.js',
-        'angular2/bundles/router.dev.js'
+        '@angular/core/bundles/core.umd.js',
+        '@angular/common/bundles/common.umd.js',
+        '@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/http/bundles/http.umd.js',
+        '@angular/router/bundles/router.umd.js',
+        '@angular/forms/bundles/forms.umd.js',
+        "rxjs/**",
+        'angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     ], { cwd: "node_modules/**" }) /* Glob required here. */
         .pipe(gulp.dest("build/node_modules"));
 });
